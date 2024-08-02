4ffcca7110620315f90956135fc6b7a76f5ff8e1
abdullox <abdulloxislomov39@gmail.com>
2024-08-02 17:25:41 +0500

    cors

--- Conf/settings.py
+++ Conf/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = ['http://subdomain.qadimiytoshkent.uz']
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'language-code',
@@ -87,11 +87,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8017",
-    "http://localhost:8017",
-    # Boshqa ruxsat etilgan manzillarni bu yerga qo'shing
     "http://subdomain.qadimiytoshkent.uz",
-    "https://subdomain.qadimiytoshkent.uz",
 ]
 
 
