92792ec6448149693aa971f4261395318d10fa97
abdullox <abdulloxislomov39@gmail.com>
2024-08-07 11:55:59 +0500

    cors

--- Conf/settings.py
+++ Conf/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = ['http://subdomain.qadimiytoshkent.uz']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'language-code',
@@ -87,9 +87,11 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8017",
+    "http://localhost:8017",
     "http://subdomain.qadimiytoshkent.uz",
+    "https://subdomain.qadimiytoshkent.uz",
 ]
-
 
 GOOGLE_REDIRECT_URL = 'https://example.com/accounts/google/login/callback/'  # or your actual callback URL
 REST_AUTH_REGISTER_SERIALIZERS = {
