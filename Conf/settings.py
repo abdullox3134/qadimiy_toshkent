cdb87c94c5ef3407d0e5e6d90f30a53eb57812f1
FarhodGanijonov <farhodganijonov81@gmail.com>
2024-08-14 17:48:24 +0500

    'db'

--- Conf/settings.py
+++ Conf/settings.py
@@ -162,24 +162,24 @@ WSGI_APPLICATION = 'Conf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'toshkent',
-        'USER': 'toshkent_user',
-        'PASSWORD': 'toshkent_password',
-        'HOST': '10.254.0.2',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'toshkent',
+#         'USER': 'toshkent_user',
+#         'PASSWORD': 'toshkent_password',
+#         'HOST': '10.254.0.2',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
